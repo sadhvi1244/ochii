db8add3015453d0d8a16345597f760dece27aa0d
Sadhvi kesarwani <Sadhvikesarwani1@gmail.com>
2025-02-17 23:47:36 +0530

    update it

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import LandingPage from "./components/LandingPage";
 import Navbar from "./components/Navbar";
 import Marquee from "./components/Marquee";
 import About from "./components/About";
-import Eyes from "./components/Eyes";
 import Features from "./components/Features";
 import Cards from "./components/Cards";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -13,9 +13,9 @@ const App = () => {
       <LandingPage />
       <Marquee />
       <About />
-      <Eyes />
       <Features />
       <Cards />
+      <Footer />
     </div>
   );
 };
